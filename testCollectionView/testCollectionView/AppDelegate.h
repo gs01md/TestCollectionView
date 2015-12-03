75dd21747e8931608c27fd2d240f6ffcf2454b39
gs01md <gs01md@163.com>
2015-12-03 09:07:41 +0800

    初始程序
    初始并添加 UICollectionView

---
+++ testCollectionView/testCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCollectionView
+//
+//  Created by WoodGao on 15/12/2.
+//  Copyright © 2015年 wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
